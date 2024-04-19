7a1db2c732e41f5314a04bf3c24bd5046cda0d80
maheswara4 <maheswara.maddireddy@gmail.com>
2024-04-19 17:49:12 +0530

    function

--- 08-function.sh
+++ 08-function.sh
@@ -11,6 +11,14 @@ else
     echo "$2 install success"
 fi
 }
+if [$user -eq 0]
+then
+    echo "please the run script with root access"
+    exit 1
+else
+    echo "you are super user"
+if
+
 
 dnf install mysql -y
 VALIDATE $? "install mysql"
